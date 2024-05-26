46c5fd766bc33e9f30c63012ff980330a0c687c7
Elliot Pettingale <elliot@smartercoding.co.uk>
2024-05-26 16:47:03 +0100

    test in page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,18 @@
-export default function Slug() {
+import { mongoConnect } from "@/lib/database";
+import Entries from "@/models/Entries";
+
+export default async function Slug() {
+  await mongoConnect();
+  const entries = await Entries.find({});
+
   return (
     <main>
       <h2>Slug</h2>
+      <div>
+        {entries.map((entry: any) => (
+            <p key={entry._id}>{entry.foo}</p>
+        ))}
+      </div>
     </main>
   );
 }
